f657a6f13b0ed48aad5ae1c53681ea4632514746
Sungwoo97 <gwaja97@gmail.com>
2024-10-29 09:31:50 +0900

    10/28 예제 업로드

--- React 예제/react/my-app/src/components/Myheader.js
+++ React 예제/react/my-app/src/components/Myheader.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 
 export default class Myheader extends Component {
-  
+  shouldComponentUpdate(){ 
+    
+      return false;
+ 
+  }
+
   render() {
+    console.log('Myheader render');
     return (
       <header>
-        <h1 className="logo">{this.props.title}</h1>
+        <h1 className="logo" onClick={()=>{
+          this.props.onChangePage();
+        }}>{this.props.title}</h1>
         <p>{this.props.desc}</p>
       </header>
     )
